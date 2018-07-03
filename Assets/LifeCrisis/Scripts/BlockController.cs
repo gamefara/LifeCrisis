7dc3736aebc2e3dea2b0ee7ca78bb5345d1065ae
HirokiOkuyama <bnr3gl1spoa8hjb3.rjqnuaiy.cose@gmail.com>
2018-07-03 13:29:59 +0900

    ・簡易ゲーム画面作成 ・1ステージ目途中まで作成 ・オブジェクト回転処理

---
+++ Assets/LifeCrisis/Scripts/BlockController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+
+	//Unity-Inspectorから指定
+	public GameObject block;
+	public float Speed;
+
+	//ブロックの移動制御
+	private Vector3 defaultPos;
+	private float defaultRadius;
+	private Vector3 centerPos;
+	private float centerAngle = 0.0f;
+	private bool isRotate = false;
+	private int jumpProcess = (int)JumpType.NoRotate;
+	private GameObject player;
+	private float blockTimer = 0.0f;
+
+	enum JumpType
+	{
+		NoRotate,
+		UpRotate,
+		DownRotate
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+		defaultPos = block.transform.position;
+		centerPos = new Vector3(0.0f, -0.5f, block.transform.position.z);
+		defaultRadius = Mathf.Pow(Mathf.Pow(defaultPos.x - centerPos.x, 2) + Mathf.Pow(defaultPos.y - centerPos.y, 2), 0.5f);
+		player = GameObject.Find("unitychan");
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(isRotate) return;
+
+		bool isUp = Input.GetKey(KeyCode.UpArrow);
+		bool isDown = Input.GetKey(KeyCode.DownArrow);
+		if(isUp || isDown)
+		{
+			if(isUp) jumpProcess = (int)JumpType.UpRotate;
+			else if(isDown) jumpProcess = (int)JumpType.DownRotate;
+			else jumpProcess = (int)JumpType.NoRotate;
+
+			StartCoroutine("RotateBlock");
+		}
+
+		var pos = block.transform.position;
+		//Time.timeのエイリアス(回転中はカウントしない)
+		blockTimer = (blockTimer + 0.01f) % (360.0f * Mathf.Deg2Rad);
+		if(block.name == "BlockMoveX")
+		{
+			//回転後移動方向も変わる
+		}
+		else if(block.name == "BlockMoveY")
+		{
+
+		}
+		else if(block.name == "BlockMoveZ") pos.z = centerPos.z + 4 * Mathf.Sin(Speed * blockTimer);
+		block.transform.position = pos;
+	}
+
+	IEnumerator RotateBlock()
+	{
+		isRotate = true;
+		for(int i = 0; i < 10; i++) yield return null;
+
+		var rotateCount = 30;
+		float addAngle = (jumpProcess == (int)JumpType.UpRotate ? -1 : 1) * 90.0f / rotateCount;
+
+		var pos = block.transform.position;
+		var squareX = Mathf.Pow(pos.x - centerPos.x, 2);
+		var squareY = Mathf.Pow(pos.y - centerPos.y, 2);
+		var radius = Mathf.Pow(squareX + squareY, 0.5f);
+		float radian;
+		var defaultRadian = Mathf.PI / 2.0f;
+		for(int i = 0; i < rotateCount; i++)
+		{
+			//オブジェクト回転
+			if(jumpProcess != (int)JumpType.NoRotate) centerAngle += addAngle;
+			block.transform.rotation = Quaternion.Euler(0, 0, centerAngle);
+
+			//オブジェクト座標移動
+			radian = centerAngle * Mathf.Deg2Rad + defaultRadian;
+			pos.x = centerPos.x + radius * Mathf.Cos(radian);
+			pos.y = centerPos.y + radius * Mathf.Sin(radian);
+			block.transform.position = pos;
+			yield return null;
+		}
+
+		var playerScript = player.GetComponent<UnityChanController>();
+		while(!playerScript.isGround) yield return null;
+		isRotate = false;
+	}
+}
---
+++ Assets/LifeCrisis/Scripts/UnityChanController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour
+{
+
+	Rigidbody body;
+	private Animator animator;
+
+	private const string parameterRun = "isRun";
+	private const string parameterJump = "isJump";
+	private const string parameterDamage = "isDamage";
+	public bool isGround = true;
+	public float speed = 2.0f;
+	public float jumpPower = 60.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.body = GetComponent<Rigidbody>();
+		this.animator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(!isGround) return;
+
+		bool isLeft = Input.GetKey(KeyCode.LeftArrow);
+		bool isRight = Input.GetKey(KeyCode.RightArrow);
+		bool isUp = Input.GetKey(KeyCode.UpArrow);
+		bool isDown = Input.GetKey(KeyCode.DownArrow);
+		bool isJump = Input.GetKey(KeyCode.Space);
+		bool isRun = (isLeft || isRight);
+		bool isRotate = (isUp || isDown);
+
+		//左右移動
+		if(isRun)
+		{
+			var v = body.velocity;
+			v.z = speed * (isLeft ? -1 : 1);
+			body.velocity = v;
+			transform.rotation = Quaternion.Euler(0, (isLeft ? 180 : 0), 0);
+			animator.SetBool(parameterRun, true);
+		}
+		else animator.SetBool(parameterRun, false);
+
+		//ジャンプ
+		if(isJump || isRotate)
+		{
+			if(animator.GetBool(parameterJump)) return;
+			body.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+		}
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		string tag = col.gameObject.tag;
+		if(tag == "Block")
+		{
+			isGround = true;
+			animator.SetBool(parameterJump, false);
+			var v = body.velocity;
+			v.x = 0;
+			body.velocity = v;
+		}
+		else if(tag == "DamageObject")
+		{
+			if(animator.GetBool(parameterDamage)) return;
+			StartCoroutine("DamageProcess");
+		}
+	}
+
+	void OnCollisionExit(Collision col)
+	{
+		string tag = col.gameObject.tag;
+		if(tag == "Block")
+		{
+			isGround = false;
+			animator.SetBool(parameterRun, false);
+			animator.SetBool(parameterJump, true);
+		}
+		else if(tag == "DamageObject")
+		{
+			isGround = false;
+			animator.SetBool(parameterRun, false);
+			animator.SetBool(parameterJump, false);
+		}
+	}
+
+	IEnumerator DamageProcess()
+	{
+		animator.SetBool(parameterDamage, true);
+		for(int i = 0; i < 60; i++)
+		{
+			yield return null;
+		}
+		animator.SetBool(parameterDamage, false);
+	}
+}
