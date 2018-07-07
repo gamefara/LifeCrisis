6a563c34fa6789fa827af1abf4f43531b8c8e11a
HirokiOkuyama <bnr3gl1spoa8hjb3.rjqnuaiy.cose@gmail.com>
2018-07-07 12:45:26 +0900

    no message

---
+++ Assets/LifeCrisis/Scripts/ButtonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour {
+
+
+	//Click Event
+	public void OnClick(){
+		string name = gameObject.name;
+		if(name == "StartButton"){
+			//Stage移行
+			SceneManager.LoadScene("Stage1");
+		}
+		else if(name == "ClearButton"){
+			
+			string sceneName = SceneManager.GetActiveScene().name;
+			if(sceneName == "Stage1") SceneManager.LoadScene("Stage2");
+			else if(sceneName == "Stage2") SceneManager.LoadScene("Stage3");
+			else if(sceneName == "Stage3") SceneManager.LoadScene("Title");
+		}
+	}
+}
